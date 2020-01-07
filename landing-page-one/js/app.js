d3122de54ea49aeca3586eb47403a406b3f57117
yeasirahamed <yahamed007@gmail.com>
2020-01-07 14:30:50 +0600

    add eventlistner

--- landing-page-one/js/app.js
+++ landing-page-one/js/app.js
@@ -1,2 +1,10 @@
-const header = document.getElementsByTagName('header');
-console.log(header);
+let whatWeDoInfo = document.querySelectorAll('.whatwedo-info');
+whatWeDoInfo.forEach(function (item) {
+    item.addEventListener('mouseleave', function (e) {
+        e.target.style.backgroundColor = "#ffffff";
+    });
+    item.addEventListener('mouseenter', function (e) {
+        e.target.style.backgroundColor = "red";
+    })
+
+})
