7d8b88b15c4341c22b3128e10df9bfabd04cbda3
yeasirahamed <yahamed007@gmail.com>
2020-01-09 01:34:05 +0600

    update nav

--- landing-page-one/js/app.js
+++ landing-page-one/js/app.js
@@ -1,15 +1,18 @@
 const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.mainnav');
-    const navitem = nav.querySelectorAll('.navitem');
-    burger.addEventListener('click', function () {
-        nav.classList.toggle('open');
-        gsap.from(navitem, {
-            opacity: 0,
-            x: 200,
-            duration: 2,
-            ease: "elastic.out(1, 0.3)"
-        })
-    })
-}
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".mainnav");
+  const navitem = nav.querySelectorAll(".navitem");
+  burger.addEventListener("click", function() {
+    nav.classList.toggle("open");
+    navitem.forEach((item, index) => {
+      gsap.from(item, {
+        opacity: 0,
+        x: 200,
+        duration: 1,
+        ease: "elastic.out(1, 0.3)",
+        delay: index * 0.1
+      });
+    });
+  });
+};
 navSlide();
