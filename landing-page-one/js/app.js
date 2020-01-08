b6235ddc44fa89c3857204a3d9185dc472286a66
yeasirahamed <yahamed007@gmail.com>
2020-01-08 23:53:57 +0600

    updating nav

--- landing-page-one/js/app.js
+++ landing-page-one/js/app.js
@@ -1,3 +1,6 @@
 const burger = document.querySelector('.burger');
 const nav = document.querySelector('.mainnav');
 const navitem = nav.querySelectorAll('.navitem');
+burger.addEventListener('click', function () {
+    nav.classList.toggle('open');
+})
