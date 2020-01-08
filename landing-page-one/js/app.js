26eae1f622ea805377a92c8020607720bc3d1efb
yeasirahamed <yahamed007@gmail.com>
2020-01-08 22:17:18 +0600

    updating nav

--- landing-page-one/js/app.js
+++ landing-page-one/js/app.js
@@ -1,10 +1,3 @@
-let whatWeDoInfo = document.querySelectorAll('.whatwedo-info');
-whatWeDoInfo.forEach(function (item) {
-    item.addEventListener('mouseleave', function (e) {
-        e.target.style.backgroundColor = "#ffffff";
-    });
-    item.addEventListener('mouseenter', function (e) {
-        e.target.style.backgroundColor = "red";
-    })
-
-})
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.mainnav');
+const navitem = nav.querySelectorAll('.navitem');
