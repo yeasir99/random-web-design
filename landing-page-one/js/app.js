ca3aeb4d8832fa79df44092f0ba2b440e45c9c28
yeasirahamed <yahamed007@gmail.com>
2020-01-09 17:49:55 +0600

    main header animation

--- landing-page-one/js/app.js
+++ landing-page-one/js/app.js
@@ -2,7 +2,7 @@ const navSlide = () => {
   const burger = document.querySelector(".burger");
   const nav = document.querySelector(".mainnav");
   const navitem = nav.querySelectorAll(".navitem");
-  burger.addEventListener("click", function() {
+  burger.addEventListener("click", function () {
     nav.classList.toggle("open");
     navitem.forEach((item, index) => {
       gsap.from(item, {
@@ -17,20 +17,39 @@ const navSlide = () => {
 
     const navLinks = document.querySelectorAll(".linkitem li");
     navLinks.forEach((item, index) => {
-      item.addEventListener("mouseenter", function() {
+      item.addEventListener("mouseenter", function () {
         gsap.to(item, {
           scale: 1.3
         });
-        item.addEventListener("mouseleave", function() {
+        item.addEventListener("mouseleave", function () {
           gsap.to(item, {
             scale: 1
           });
         });
       });
     });
+
   });
 };
+
 navSlide();
+
+const headerAnimation = () => {
+  const mainLogo = document.querySelector(".logo");
+  gsap.from(mainLogo, {
+    y: -100,
+    ease: "elastic.out(1, 0.3)",
+    delay: 1.5
+  });
+  const burger = document.querySelector(".burger");
+  gsap.from(burger, {
+    y: -100,
+    ease: "elastic.out(1, 0.3)",
+    delay: 1.5
+  })
+}
+
+headerAnimation();
 
 //mail validation
 
